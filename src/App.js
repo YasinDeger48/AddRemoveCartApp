3a15e1f6e0415e89712ce5a3e496949b7d7ce11a
Yasin Değer <letsexploreb31@gmail.com>
2021-12-23 17:04:07 +0300

    App correction

--- src/App.js
+++ src/App.js
@@ -13,18 +13,8 @@ const App = (props) => {
 <h1>
 <img src="https://avatars.githubusercontent.com/u/8417720?v=4" alt="Picture Alt"/>
   K<span>itapçı</span>M 
-
-
 </h1>
 
-      {/* <h1>
-        Alışveriş Sepeti Yapımı
-        <img
-          src="https://avatars3.githubusercontent.com/u/60869810?v=4"
-          alt="React Dersleri"
-        />{" "}
-        React Dersleri
-      </h1> */}
       <Route exact path="/" component={Products} />
       <Route path="/cart" component={Cart} />
     </div>
